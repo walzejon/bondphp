e080a30d1420898356bd063e97735333606e6ac8
stlp <stlp>
2021-03-12 15:08:31 -0800

    header.php

--- header.inc.php
+++ header.inc.php
@@ -5,6 +5,9 @@
 <div class="topnav">
     <a href="index.php">Home</a>
     <a href="list_owners.php">List Owners</a>
+    <!-- add the rest of the nav bar links below with correct file names and navbar names
+     ex:-->
     <!--<a href="list_products.php">List Products</a> -->
+
 </div></div>
 <h1>Sample PHP Database Program</h1>
