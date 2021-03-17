f3154992282e5c396719e5f6024f318f41896a2d
Benjamin L <31552382+softpotato@users.noreply.github.com>
2021-03-17 12:36:51 -0700

    Restoring Files

--- list_stock.php
+++ list_stock.php
@@ -12,14 +12,15 @@ if ($id === "" OR $id === false OR $id === null) {
     exit();
 }
 ?>
-
+<!DOCTYPE html>
 <html>
     <head>
         <title>Stock Database Program</title>
+        <link rel="stylesheet" type="text/css" href="base.css?v=<?php echo time(); ?>"/>
     </head>
     <body>
         <div>
-            <h2>List Stocks Owned by Owner (Testing Query)</h2>
+            <h2>List Stocks Owned by Owner</h2>
             <?php
             // Initializing SQL connection
             $conn = new mysqli($servername, $username, $password, $database, $port);
@@ -28,9 +29,20 @@ if ($id === "" OR $id === false OR $id === null) {
             }
 
             // SQL Query
-            $sql = "SELECT O.ownerid, O.firstName, O.lastName, O.email, O.phoneNumber, A.accountNum FROM Owner O INNER JOIN (OwnerAccount OA "
-            . "INNER JOIN Account A USING (accountNum)) "
-            . "USING (ownerid) WHERE O.ownerid = ?";
+            $sql = "SELECT O.ownerid, O.firstName, O.lastName, O.email, O.phoneNumber, A.accountNum, A.accountCreationDate, ASS.quantity, S.stockId,"
+            . " S.currentprice, C.tickerSymbol, C.companyName FROM Owner O"
+            . " INNER JOIN ("
+                . " OwnerAccount OA INNER JOIN ("
+                    . " Account A INNER JOIN ("
+                        . " AccountStock ASS INNER JOIN ("
+                            . " Stock S INNER JOIN ("
+                                . " Company C"
+                            . " ) ON S.companyId = C.companyId"
+                        . " ) ON ASS.stockid = S.stockid"
+                    . " ) ON A.accountNum = ASS.accountNum"
+                . " ) ON OA.accountNum = A.accountNum"
+            . " ) ON O.ownerID = OA.ownerID"
+            . " WHERE O.ownerid = ?";
             
             
             $stmt = $conn->stmt_init();
@@ -39,17 +51,40 @@ if ($id === "" OR $id === false OR $id === null) {
             } else {
                 $stmt->bind_param('s', $id);
                 $stmt->execute();
-                $stmt->bind_result($ownerid, $firstName, $lastName, $email, $phoneNumber);
-                echo "<div>";
+                $stmt->bind_result($ownerid, $firstName, $lastName, $email, $phoneNumber, $accountNum, $accountCreationDate, $quantity, $stockid,
+                                    $currentprice, $tickersymbol, $companyname);
+                echo "<div><table>";
+
+                echo 
+                "<tr>
+                    <th>Owner ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Phone Number</th>
+                    <th>Account Creation Date</th>
+                    <th>Quantity</th>
+                    <th>Stock ID</th>
+                    <th>Current Price</th>
+                    <th>Stock Ticker Symbol</th>
+                    <th>Company Name</th>
+                </tr>";
+
                 while ($stmt->fetch()) {
-                    echo $ownerid . ', ' . $firstName . ', ' . $lastName . ', ' . $email . ', ' . $phoneNumber . '<br>'; 
+                    echo '<tr><td>' . $ownerid . '</td><td>' . $firstName . '</td><td>' . $lastName . '</td><td>' . $email . '</td><td>' . $phoneNumber
+                    . $accountNum . '</td><td>' . $accountCreationDate . '</td><td>' . $quantity . '</td><td>' . $stockid . '</td><td>' . 
+                    $currentprice . '</td><td>' . $tickersymbol . '</td><td>' . $companyname . '</td></tr>'; 
                 }
-                echo "</div>";
+                echo "<table></div>";
             }
 
             // Closing SQL connection
             $conn->close();
             ?>
+        </div>
+        <div>
+            <br>
+            <a href="list_owners.php"><-Return</a>
         </div>
     </body>
 </html>
